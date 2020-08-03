cd05d09582246fb885db406328f7a013da7ca917
sabrina <dias.sabrina@gmail.com>
2020-08-02 22:23:14 -0300

    lint ajustment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,43 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  env: {
-    es6: true,
-    node: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "airbnb-base",
-    "plugin:import/errors",
-    "prettier",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "prettier/@typescript-eslint",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    project: "./tsconfig.json",
-    "createDefaultProgram": true
-  },
-  rules: {
-    // Recomended Rules
-    "max-len": ["error", 120],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        ts: "never",
-      },
+    "parser": "@typescript-eslint/parser",
+    "env": {
+      "es6": true,
+      "node": true,
+      "mocha": true,
+    },
+    "plugins": [
+      "@typescript-eslint",
     ],
-  },
+    "extends": [
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "airbnb-base",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript",
+    ],
+    "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly",
+    },
+    "parserOptions": {
+      "project": "./tsconfig.json"
+    },
+    "rules": {
+      // Recomended Rules
+      "max-len":["error", 120],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          "js": "never",
+          "jsx": "never",
+          "ts": "never",
+          "tsx": "never"
+        }
+     ]
+    }
 };
